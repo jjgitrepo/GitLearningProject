4063f801f1319db0768eb13c2e2691ad9457708d
JagadeeshBabu <jagadeeshbabu282@gmail.com>
2019-12-15 23:15:17 +0530

    Good morning message is added

--- src/com/jjsoft/HelloGit1.java
+++ src/com/jjsoft/HelloGit1.java
@@ -3,6 +3,8 @@ package com.jjsoft;
 public class HelloGit1 {
 
 	public static void main(String[] args) {
+		
+		System.out.println("Good Morning..."+args[0]);
 		System.out.println("Welcome to git world....Happy Learning... second file");
 	}
 
