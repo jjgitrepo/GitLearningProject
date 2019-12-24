4f86fb911d0dc26558f7a5cbdc3c110c2a55b2e2
JagadeeshBabu <jagadeeshbabu282@gmail.com>
2019-12-24 19:42:19 +0530

    cleaning up the class

--- src/com/jjsoft/HelloGit.java
+++ src/com/jjsoft/HelloGit.java
@@ -5,18 +5,6 @@ public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("Welcome to git world....Happy Learning.....by Harshit.. from master branch");
 		
-		System.out.println("after feature4 master1 commit1");
-		System.out.println("after feature4 master1 commit1");
-		System.out.println("after feature4 master1 commit1");
-
-		System.out.println("after feature4 master1 commit2");
-		System.out.println("after feature4 master1 commit2");
-		System.out.println("after feature4 master1 commit2");
-		
-		System.out.println("feature5   .1....  commit1");
-		System.out.println("feature5  .1 ....  commit1");
-
-
-	}
+			}
 
 }
