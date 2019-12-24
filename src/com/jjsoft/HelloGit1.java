81649a9f77868540f4191305c879ca6d12b6d251
JagadeeshBabu <jagadeeshbabu282@gmail.com>
2019-12-24 19:43:43 +0530

    cleaning up the class in master

--- src/com/jjsoft/HelloGit1.java
+++ src/com/jjsoft/HelloGit1.java
@@ -5,30 +5,9 @@ public class HelloGit1 {
 	public static void main(String[] args) {
 		
 		System.out.println("Good Morning..."+args[0]);
-		System.out.println("Welcome to git world....Happy Learning... second file");
-		
-		System.out.println("new featue of FeatureBranch2");
-		System.out.println("new featue of FeatureBranch2");
 		
 		
-		System.out.println("new featue of FeatureBranch3");
-		System.out.println("new featue of FeatureBranch3");
-		
-		
-		System.out.println("new featue1 of FeatureBranch3");
-		System.out.println("new featue1 of FeatureBranch3");
-		
-		System.out.println("new featue1 of FeatureBranch4");
-		System.out.println("new featue1 of FeatureBranch4");
-		
-		System.out.println("new featue1 of FeatureBranch4");
-		System.out.println("new featue1 of FeatureBranch4");
-		
-		System.out.println("new featue of FeatureBranch5");
-		System.out.println("new featue of FeatureBranch5");
-		
-		
-		System.out.println("master 5===================");
+		System.out.println("Cleaning up master");
 		
 		
 	}
