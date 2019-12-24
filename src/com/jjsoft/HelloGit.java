039fd9cccde80c178fac582ac8b4cb4e36fb91de
JagadeeshBabu <jagadeeshbabu282@gmail.com>
2019-12-24 20:35:47 +0530

    Rebase conflicts in Master ....

--- src/com/jjsoft/HelloGit.java
+++ src/com/jjsoft/HelloGit.java
@@ -3,7 +3,13 @@ package com.jjsoft;
 public class HelloGit {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to git world....Happy Learning.....");
+		System.out.println("Welcome to git world....Happy Learning.....11");
+		System.out.println("Welcome to git world....Happy Learning.....22");
+		System.out.println("Welcome to git world....Happy Learning.....33");
+		System.out.println("Welcome to git world....Happy Learning.....44");
+		System.out.println("Welcome to git world....Happy Learning.....55");
+		System.out.println("Welcome to git world....Happy Learning.....66");
+		
 		
 	}
 
