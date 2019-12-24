1f7b85caacb28f13c4f73a356389880722f96eb3
JagadeeshBabu <jagadeeshbabu282@gmail.com>
2019-12-24 20:34:33 +0530

    Rebase conflicts in branch....

--- src/com/jjsoft/HelloGit.java
+++ src/com/jjsoft/HelloGit.java
@@ -3,6 +3,13 @@ package com.jjsoft;
 public class HelloGit {
 
 	public static void main(String[] args) {
+		System.out.println("Welcome to git world....Happy Learning.....");
+		System.out.println("Rebasing conflicts....with Git version1");
+		System.out.println("Rebasing conflicts....with Git version2");
+		System.out.println("Rebasing conflicts....with Git version3");
+		System.out.println("Rebasing conflicts....with Git version4");
+		System.out.println("Rebasing conflicts....with Git version5");
+
 		System.out.println("Welcome to git world....Happy Learning.....11");
 		System.out.println("Welcome to git world....Happy Learning.....22");
 		System.out.println("Welcome to git world....Happy Learning.....33");
