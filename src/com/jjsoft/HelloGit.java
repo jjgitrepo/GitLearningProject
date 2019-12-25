ad4c13edb09df92ff79862be6bbb2d9dfc5665ee
JagadeeshBabu <jagadeeshbabu282@gmail.com>
2019-12-25 13:10:59 +0530

    Going to start stashing...example

--- src/com/jjsoft/HelloGit.java
+++ src/com/jjsoft/HelloGit.java
@@ -4,21 +4,7 @@ public class HelloGit {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to git world....Happy Learning.....");
-		System.out.println("Rebasing conflicts....with Git version1");
-		System.out.println("Rebasing conflicts....with Git version2");
-		System.out.println("Rebasing conflicts....with Git version3");
-		System.out.println("Rebasing conflicts....with Git version4");
-		System.out.println("Rebasing conflicts....with Git version5");
-
-		System.out.println("Welcome to git world....Happy Learning.....11");
-		System.out.println("Welcome to git world....Happy Learning.....22");
-		System.out.println("Welcome to git world....Happy Learning.....33");
-		System.out.println("Welcome to git world....Happy Learning.....44");
-		System.out.println("Welcome to git world....Happy Learning.....55");
-		System.out.println("Welcome to git world....Happy Learning.....66");
-		
-		System.out.println("after rebase merge conflicts.....1");
-		
+		System.out.println("Demonstration for git stashing...");
 		
 	}
 
